ca351afa3ca9b7d3fb74c2bf34bf0ee5712f9d90
LukeT <luketabinor@icloud.com>
2025-03-29 21:44:13 +0000

    files

--- src/main/java/com/flashlights/Flashlights.java
+++ src/main/java/com/flashlights/Flashlights.java
@@ -1,7 +1,9 @@
 package com.flashlights;
 
+import com.flashlights.network.FlashlightTogglePacket;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,14 +13,10 @@ public class Flashlights implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		FlashlightTogglePacket.register();
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
