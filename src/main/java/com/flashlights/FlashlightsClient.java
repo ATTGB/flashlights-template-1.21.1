186f1ef40c7a9e010b70a77d2c23f2f75bf6b657
LukeT <luketabinor@icloud.com>
2025-03-29 21:29:27 +0000

    files

---
+++ src/main/java/com/flashlights/FlashlightsClient.java
@@ -0,0 +1,47 @@
+package com.flashlights;
+
+import com.flashlights.keybinds.KeybindsManager;
+import com.flashlights.light.LightManager;
+import foundry.veil.api.event.VeilRenderLevelStageEvent;
+import foundry.veil.fabric.event.FabricVeilRenderLevelStageEvent;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
+
+import java.util.UUID;
+
+public class FlashlightsClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+        KeybindsManager.register();
+
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
+            UUID playerUuid = MinecraftClient.getInstance().getSession().getUuidOrNull();
+            if (playerUuid == null) return;
+            LightManager.onPlayerDisconnected(playerUuid);
+        });
+
+        ClientPlayConnectionEvents.JOIN.register((handler, client, isFirstJoin) -> {
+            UUID playerUuid = MinecraftClient.getInstance().getSession().getUuidOrNull();
+            if (playerUuid != null) {
+                LightManager.onPlayerRejoined(playerUuid);
+            }
+        });
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            KeybindsManager.handleFlashlightToggle();
+            LightManager.removeInactiveFlashlights();
+        });
+
+        FabricVeilRenderLevelStageEvent.EVENT.register((stage, levelRenderer, bufferSource, matrixStack, frustumMatrix, projectionMatrix, renderTick, deltaTracker, camera, frustum) -> {
+            if (stage == VeilRenderLevelStageEvent.Stage.AFTER_LEVEL) {
+                LightManager.updateFlashlights();
+            }
+        });
+    }
+}
+
+
---
+++ src/main/java/com/flashlights/keybinds/KeybindsManager.java
@@ -0,0 +1,37 @@
+package com.flashlights.keybinds;
+
+import com.flashlights.light.LightManager;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class KeybindsManager {
+    public static final KeyBinding toggleFlashlight = new KeyBinding(
+            "key.flashlight.toggle_flashlight",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_G,
+            "category.flashlight"
+    );
+
+
+    public static void register() {
+        KeyBindingHelper.registerKeyBinding(toggleFlashlight);
+    }
+
+    public static boolean isFlashlightTogglePressed() {
+        return toggleFlashlight.wasPressed();
+    }
+
+    public static void handleFlashlightToggle() {
+        if (isFlashlightTogglePressed()) {
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            if (player != null) {
+                LightManager.toggleDebugEnabled(player.getUuid());
+                LightManager.updateFlashlights();
+            }
+        }
+    }}
+
