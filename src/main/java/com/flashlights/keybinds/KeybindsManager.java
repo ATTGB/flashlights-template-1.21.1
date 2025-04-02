e68962622ed8bf19e396aebe95965855cdb273ec
LukeT <luketabinor@icloud.com>
2025-04-02 20:00:53 +0100

    files

--- src/main/java/com/flashlights/keybinds/KeybindsManager.java
+++ src/main/java/com/flashlights/keybinds/KeybindsManager.java
@@ -1,6 +1,7 @@
 package com.flashlights.keybinds;
 
 import com.flashlights.light.LightManager;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -16,11 +17,12 @@ public class KeybindsManager {
             "category.flashlight"
     );
 
-
     public static void register() {
         KeyBindingHelper.registerKeyBinding(toggleFlashlight);
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (toggleFlashlight.wasPressed()) {
+                LightManager.toggleFlashlight();
+            }
+        });
     }
-
-
-    }
-
+}
--- src/main/java/com/flashlights/light/LightManager.java
+++ src/main/java/com/flashlights/light/LightManager.java
@@ -1,5 +1,3 @@
-
-
 package com.flashlights.light;
 
 import com.flashlights.Item.items.FlashLightItem;
@@ -15,20 +13,19 @@ import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3d;
 import org.joml.Quaternionf;
 import org.joml.Vector3d;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 public class LightManager {
     public static final HashMap<Integer, AreaLight> ITEM_LIGHT = new HashMap<>();
     public static final int DEFAULT_LIGHT_COLOR = 0xFFf0d3d3;
     public static final int WHITE_LIGHT_COLOR = 0xFFf0f0f0;
+
+    private static boolean isFlashlightOn = false; // Track flashlight state
 
     public static void init() {
         VeilEventPlatform.INSTANCE.onVeilRenderLevelStage((stage, levelRenderer, bufferSource, matrixStack, frustumMatrix, projectionMatrix, renderTick, deltaTracker, camera, frustum) -> {
@@ -37,22 +34,19 @@ public class LightManager {
                     tickPlayerFlashlight(player, deltaTracker);
                 }
             }
-
         });
     }
 
+    public static void toggleFlashlight() {
+        isFlashlightOn = !isFlashlightOn; // Toggle flashlight state
+    }
+
     private static void tickPlayerFlashlight(PlayerEntity player, RenderTickCounter deltaTracker) {
-
-        // TODO if player has a flashlight in hand else set delete his light
-        if (player.getMainHandStack().getItem() instanceof FlashLightItem || player.getOffHandStack().getItem() instanceof FlashLightItem) {
-
+        if (isFlashlightOn && (player.getMainHandStack().getItem() instanceof FlashLightItem || player.getOffHandStack().getItem() instanceof FlashLightItem)) {
             Hand hand = (player.getOffHandStack().getItem() instanceof FlashLightItem) ? Hand.OFF_HAND : Hand.MAIN_HAND;
-
             ItemStack stack = player.getStackInHand(hand);
-
             AreaLight light = new AreaLight();
             Vec3d renderPosition = player.getLerpedPos(deltaTracker.getTickDelta(false));
-            Vec3d eyepos = player.getEyePos();
 
             if (ITEM_LIGHT.containsKey(player.getId())) {
                 light = ITEM_LIGHT.get(player.getId());
@@ -60,7 +54,7 @@ public class LightManager {
                 light.setBrightness(2.0f);
 
                 int color = DEFAULT_LIGHT_COLOR;
-                if (stack.get(ModComponents.COLOR)!=null) {
+                if (stack.get(ModComponents.COLOR) != null) {
                     color = ColorUtils.blendColors(DEFAULT_LIGHT_COLOR, stack.get(ModComponents.COLOR));
                 }
 
@@ -70,41 +64,33 @@ public class LightManager {
                 light.setDistance(20.0f);
                 light.setAngle(0.6f);
 
-
                 ITEM_LIGHT.put(player.getId(), light);
                 VeilRenderSystem.renderer().getLightRenderer().addLight(light);
             }
-            light.setPosition(renderPosition.x, renderPosition.y+1.5, renderPosition.z);
+            light.setPosition(renderPosition.x, renderPosition.y + 1.5, renderPosition.z);
             light.setOrientation(OrientationUtils.getOrientation(player.getRotationVector()));
 
             int color = DEFAULT_LIGHT_COLOR;
-            if (stack.get(ModComponents.COLOR)!=null) {
+            if (stack.get(ModComponents.COLOR) != null) {
                 color = ColorUtils.blendColors(DEFAULT_LIGHT_COLOR, stack.get(ModComponents.COLOR));
             }
 
-
             light.setColor(color);
 
-            // compoent on/off
-            if (stack.get(ModComponents.IS_ON)!=null) {
+            if (stack.get(ModComponents.IS_ON) != null) {
                 if (!stack.get(ModComponents.IS_ON)) {
                     light.setBrightness(2.0f);
                     light.setPosition(player.getX(), player.getY() + 1.6f, player.getZ());
-
                 } else {
                     light.setBrightness(0.0f);
                 }
             } else {
                 light.setBrightness(0.0f);
             }
-
-
-
-        } else if (ITEM_LIGHT.containsKey(player.getId())){
+        } else if (ITEM_LIGHT.containsKey(player.getId())) {
             AreaLight light = ITEM_LIGHT.get(player.getId());
             VeilRenderSystem.renderer().getLightRenderer().removeLight(light);
             ITEM_LIGHT.remove(player.getId());
         }
-
     }
 }
