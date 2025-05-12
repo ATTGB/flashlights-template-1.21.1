d0e262da4152405c863c75f0f85164417349ffda
LukeT <luketabinor@icloud.com>
2025-05-12 14:57:52 +0100

    files

--- src/main/java/com/flashlights/Flashlights.java
+++ src/main/java/com/flashlights/Flashlights.java
@@ -2,23 +2,12 @@ package com.flashlights;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Flashlights implements ModInitializer {
-	public static final String MOD_ID = "flashlights";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        VeilRenderSystem.registerRenderTask(RenderPhase.LAST, (context, buffer) -> {
+        // Draw a red rectangle at (50, 50) with size (100, 100)
+        context.fill(buffer, 50, 50, 150, 150, 0xFFFF0000);
+    });
+}
 }
